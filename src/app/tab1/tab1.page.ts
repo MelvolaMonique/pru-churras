fca40f2a9e7a5c5acd71ae1da4b196717cf4bcd2
MelvolaMonique <monique.lima10@etec.gov.br>
2023-03-09 12:29:45 -0300

    colocando animação 2/2
    terminando de colocar animação

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,31 +1,46 @@
+import { animate, state, style, transition, trigger } from '@angular/animations'
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  animations: [
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotateY(0) ' })),
+      state('rotated', style({ transform: 'rotateY(-360deg)' })),
+      transition('rotated => default', animate('2000ms ease-out')),
+      transition('default => rotated', animate('2000ms ease-in'))
+    ])
+  ]
+
 })
 export class Tab1Page {
 
-  cara = 'asstes/cara.png';
-  coroa = 'asstes/coroa.png';
-  logo = 'asstes/logo.png';
+  cara = 'assets/cara.png';
+  coroa = 'assets/coroa.png';
+  logo = 'assets/logo.png';
   image = this.logo;
   info = 'Clique no botão para jogar!';
-
+  state = 'default';
 
   constructor() { }
 
-  jogarMoeda(){
-    if(Math.random() < 0.5){
-      this.image = this.cara;
-      this.info = "Cara!"
-    } else{
-      this.image = this.coroa;
-      this.info = "Coroa!"
-    }
+  jogarMoeda() {
+    this.info = 'Girando...';
+    this.image = this.logo;
+    this.state = this.state === 'default' ? 'rotated' : 'default';
 
+    setTimeout(()=>{
+
+      if (Math.random() < 0.5) {
+        this.image = this.cara;
+        this.info = "Cara!"
+      } else {
+        this.image = this.coroa;
+        this.info = "Coroa!"
+      }
+
+    }, 2000);
   }
-
-
 }
