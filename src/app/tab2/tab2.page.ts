c535f06d05bbb412f51789a6ee1917cccd632354
MelvolaMonique <monique.lima10@etec.gov.br>
2023-03-23 12:21:35 -0300

    página do truco terminada

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,30 +10,56 @@ export class Tab2Page {
   valendo = 1;
   contarpontos1 = 0;
   contarpontos2 = 0;
+  registrar1 = 0;
+  registrar2 = 0;
 
-
-  constructor() {}
-
-
+  constructor(private alertController: AlertController) {}
   MudarValendo(numero: number ){
   this.valendo = numero;
 }
   Contador1(){
     this.contarpontos1 += this.valendo
+    if(this.contarpontos1 > 12 ){
+      this.contarpontos1 = 0;
+      this.registrar1 += 1;
+    }
   }
-
   Contador2(){
     this.contarpontos2 += this.valendo
+    if(this.contarpontos2 > 12 ){
+      this.contarpontos2 = 0;
+      this.registrar2 += 1;
+    }
   }
-
   Diminuir1(){
     this.contarpontos1 -= this.valendo
-    
+
+    if(this.contarpontos1 < 0){
+      this.contarpontos1 = 0;
+     }
 
   }
   Diminuir2(){
     this.contarpontos2 -= this.valendo
-
+    if(this.contarpontos2 < 0){
+      this.contarpontos2 = 0;
+     }
   }
 
+  Limpar(){
+    this.valendo = 1;
+    this.contarpontos1 = 0;
+    this.contarpontos2 = 0;
+    this.registrar1 = 0;
+    this.registrar2 = 0;
+
+  }
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: 'Jogo Reiniciado',
+      cssClass: 'custom-alert',
+    });
+
+    await alert.present();
+  }
 }
